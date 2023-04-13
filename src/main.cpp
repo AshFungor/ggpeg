ed32a8d94e98d3435f0710e3f121387593b3b33e
Caaaaactus <imdolgov@edu.hse.ru>
2023-04-13 18:48:35 +0300

    Первый коммит
    Описание:
    #########################
    main.cpp
    Добавленный функционал:
    Читаем параметры передаваемые программе при запуске, конвертируем их из char** в vector<string> inputTokens,
    после создаем парсер parser и передаем ему созданный вектор.
    Пока парсер возвращает vector<string>, в дальнейшем передалаю так чтобы возвращал очередь.
    #########################
    clp-parser.hpp
    Добавленный функционал:
    Объявлен класс Parser,
    Объявленно перечисление c типами CommandType,
    #########################
    clp-parser.cpp
    Добавленный функционал:
    Добавлен метод класса Parser parse возвращающий vector<string>
    #########################
    Что сейчас умеет программа:
    Вводим .\ggpeg.exe cats.ppm -nxy -h --version
    Получаем
    --negative
    --reflect_x
    --reflect_y
    --help
    --version

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
 
+#include <vector>
+
 // Local headers.
 #include <clp-parser/clp-parser.hpp>
 #include <image/image.hpp>
 #include <processing/processing.hpp>
 
-int main()
-{
+int main(int argc, char** argv)
+{   
+    // Vector with the values of the 
+    // commands given when the program starts
+    std::vector<std::string> inputTokens;
+
+    // Add tokens to vector
+    for (size_t i = 1; i < argc; ++i)
+    {
+        inputTokens.emplace_back(argv[i]);
+    }
+
+    // Path to image
+    std::string image = inputTokens[0];
+
+    clpp::Parser parser {};
+    std::vector<std::string> goodTokens = parser.parse(inputTokens);
+
+    for (size_t i = 0; i!=goodTokens.size(); ++i)
+    {
+        std::cout<<goodTokens[i]<<std::endl;
+    }
+
     return 0;
 }
