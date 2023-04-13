de4915d330790d5e3bc60d5235a5718d2a072d34
Caaaaactus <imdolgov@edu.hse.ru>
2023-04-13 18:48:35 +0300

    Первый коммит
    Описание:
    #########################
    main.cpp
    Добавленный функционал:
    Читаем параметры передаваемые программе при запуске, конвертируем их из char** в vector<string> inputTokens,
    после создаем парсер parser и передаем ему созданный вектор.
    Пока парсер возвращает vector<string>, в дальнейшем передалаю так чтобы возвращал очередь.
    #########################
    clp-parser.hpp
    Добавленный функционал:
    Объявлен класс Parser,
    Объявленно перечисление c типами CommandType,
    #########################
    clp-parser.cpp
    Добавленный функционал:
    Добавлен метод класса Parser parse возвращающий vector<string>
    #########################
    Что сейчас умеет программа:
    Вводим .\ggpeg.exe cats.ppm -nxy -h --version
    Получаем
    --negative
    --reflect_x
    --reflect_y
    --help
    --version

--- src/clp-parser/clp-parser.cpp
+++ src/clp-parser/clp-parser.cpp
@@ -1,3 +1,34 @@
 #include "clp-parser.hpp"
 
+#include <vector>
+
 // Definitions for CLI parser.
+
+
+std::vector<std::string> clpp::Parser::parse(std::vector<std::string> tokens)
+{	
+
+	std::vector<std::string> goodTokens;
+	
+
+	for (size_t i = 1; i < tokens.size(); ++i)
+	{	
+		if (tokens[i][1] == '-') // Processing a complicated command
+		{	
+			goodTokens.emplace_back(tokens[i]);
+		}
+		else // Processing a simple command
+		{
+			for (size_t j = 1; j < tokens[i].size(); ++j) // Decoding the token into complicated commands
+			{	
+				if (tokens[i][j] == 'n') { goodTokens.emplace_back("--negative"); }
+				else if (tokens[i][j] == 'x') { goodTokens.emplace_back("--reflect_x"); }
+				else if (tokens[i][j] == 'y') { goodTokens.emplace_back("--reflect_y"); }
+				else if (tokens[i][j] == 'v') { goodTokens.emplace_back("--version"); }
+				else if (tokens[i][j] == 'h') { goodTokens.emplace_back("--help"); }
+			}
+		}
+	}
+
+	return goodTokens; 
+}
--- src/clp-parser/clp-parser.hpp
+++ src/clp-parser/clp-parser.hpp
@@ -1,5 +1,49 @@
 #pragma once
 
+#include <iostream>
+#include <vector>
+#include <string>
+
 namespace clpp {
     // Code for CLI parser.
+
+    //std::string globalPathToImage{""};
+
+    // Parser class
+    // Accepts vector and returns a queue of commands
+    class Parser
+    {
+        public:
+            std::vector<std::string> parse(std::vector<std::string> tokens);
+    };
+
+    // Enum with command
+    enum CommandType
+    {
+        crop = 0, 
+        rotate = 0, 
+        resize = 0,
+        negative = 0, // n
+        insert = 0,
+        convert_to = 0,
+        reflect_x = 0, // x
+        reflect_y = 0, // y
+        version = 0, // v
+        help = 0 // h
+    };
+
+    // Command class
+    // Store type and arguments for command
+    class Command
+    {
+        public:
+            Command(std::string inputToken);
+        
+        private:
+            std::string type; // Type of command
+            std::vector<int> arg{NULL}; // Arguments for command. In default null;
+        public: 
+
+    };
+
 }
