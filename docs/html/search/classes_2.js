07caa108be9650891f2448f9a3fe00e7467f10e1
AshFungor <evgenynarkhov2@gmail.com>
2023-04-25 00:47:56 +0300

    updating docs

--- docs/html/search/classes_2.js
+++ docs/html/search/classes_2.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['pixelmap_0',['PixelMap',['../classimg_1_1PixelMap.html',1,'img']]]
+  ['parser_0',['Parser',['../classclpp_1_1Parser.html',1,'clpp']]],
+  ['pixelmap_1',['PixelMap',['../classimg_1_1PixelMap.html',1,'img']]],
+  ['ppmimage_2',['PPMImage',['../classimg_1_1PPMImage.html',1,'img']]]
 ];
