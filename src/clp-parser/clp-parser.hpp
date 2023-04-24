63f9c91b868b9043a389d109ef9fed96a94eb457
Caaaaactus <imdolgov@edu.hse.ru>
2023-04-24 19:35:07 +0300

    parser готов, осталось добавить несколько методов для обращения к полям, а также документацию и тесты

--- src/clp-parser/clp-parser.hpp
+++ src/clp-parser/clp-parser.hpp
@@ -1,35 +1,41 @@
 #pragma once
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
 #include <vector>
 #include <string>
+#include <algorithm>
+#include <queue>
 
+
+/** \brief
+ *  Namespaceclpp
+ */
 namespace clpp {
     // Code for CLI parser.
 
-    //std::string globalPathToImage{""};
+    // Path to image
+    inline std::string global_Path;
 
-    // Parser class
-    // Accepts vector and returns a queue of commands
-    class Parser
-    {
-        public:
-            std::vector<std::string> parse(std::vector<std::string> tokens);
-    };
+    // Allowed formats
+    inline std::vector<std::string> allowed_Format = {"ppm", "tga", "png"};
+
 
     // Enum with command
-    enum CommandType
+    enum class CommandType
     {
         crop = 0, 
-        rotate = 0, 
-        resize = 0,
-        negative = 0, // n
-        insert = 0,
-        convert_to = 0,
-        reflect_x = 0, // x
-        reflect_y = 0, // y
-        version = 0, // v
-        help = 0 // h
+        rotate = 1, 
+        resize = 2,
+        negative = 3, // n
+        insert = 4,
+        convert_to = 5,
+        reflect_x = 6, // x
+        reflect_y = 7, // y
+        version = 8, // v
+        help = 9 // h
     };
 
     // Command class
@@ -37,13 +43,64 @@ namespace clpp {
     class Command
     {
         public:
-            Command(std::string inputToken);
-        
+            Command(std::string& command);
+
+            void show_param();
+            
+            
         private:
-            std::string type; // Type of command
-            std::vector<int> arg{NULL}; // Arguments for command. In default null;
-        public: 
+            CommandType _command;
+            std::vector<std::string> _param;
+
+            void _parser_param_crop(std::string& second_part);
+            void _parser_param_rotate(std::string& second_part);
+            void _parser_param_resize(std::string& second_part);
+            void _parser_param_insert(std::string& second_part);
+            void _parser_param_convert_to(std::string& second_part);
+            void _exceptions_for_new_image(const std::string &path);
+            void _error_param();
 
     };
+
+    inline void helppp(){
+        std::cerr<<"help.."<<std::endl;
+    }
+
+    // Parser class
+    // Accepts vector and returns a queue of commands
+    class Parser
+    {
+        public:
+            Parser(const std::vector<std::string> &tokens);
+            ~Parser();
+            
+            std::vector<std::string> get_tokens();
+            std::vector<std::string> get_line_of_command();
+
+            void show_tokens();
+            void show_line_of_command();
+
+
+        private:
+            std::vector<std::string> _tokens;
+            std::vector<std::string> _line_of_command;
+            std::queue<Command> _queue_of_command;
+
+            // Parsing tokens in a suitable vector format
+            void _parse_to_string(); 
+            std::string _convert_to_long(const char sh);
+            void _parse_to_queue();
+
+            // Check commands
+            void _check_combination_short_command(const std::string &combination_short_command);
+            void _check_long_command(const std::string &long_command);
+
+            // Handling exceptions
+            void _exceptions_for_image(const std::string &path);
+            void _exceptions_for_short_command(const char token);
+            void _exceptions_for_incorrect_format(const std::string &token);
+
+    };
+
 
 }
---
+++ src/clp-parser/color-commandline.hpp
@@ -0,0 +1,13 @@
+// ConsoleColor
+#ifdef _WIN32
+#include <windows.h>  
+#endif
+void setConsoleColor(int color) 
+{
+    #ifdef _WIN32
+        HANDLE hConsole = GetStdHandle(STD_ERROR_HANDLE);
+        SetConsoleTextAttribute(hConsole, color);
+    #else
+        std::cerr << "\033[" << color << "m";
+    #endif
+}
