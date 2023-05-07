3be5ab1dd78ddc808fb1bba5df4fe3518b36380b
Grisha <khoroshilov@gmail.com>
2023-05-07 09:56:53 +0300

    add documentation

--- src/processing/processing.hpp
+++ src/processing/processing.hpp
@@ -3,17 +3,53 @@
 
 namespace proc {
     // Code for processing functions.
+    /** \brief Imposes a negative filter on the image.
+     * \param img The image to which the filter is applied.
+     */
     void negative(img::Image &img);
-
+    /** \brief Crops the image.
+     * \param img The image that is being cropped.
+     * \param left The number of percentages by which the image will be cropped from \a left.
+     * \param top The number of percentages by which the image will be cropped from \a top.
+     * \param right The number of percentages by which the image will be cropped from \a right.
+     * \param bottom The number of percentages by which the image will be cropped from \a bottom.
+     * \details This function does not perform any range-checking for \a left, \a top, \a right, \a bottom. 
+     * The function cuts the picture on each side depending on the parameters \a left, \a top, \a right, \a bottom.
+     * For instance, left means that \a left pixels
+     * are cut from the left.
+     */
     void crop(img::Image &img, double left, double top, double right, double bottom);
-
+    /** \brief Inserts one image into another.
+     * \param img The image on which the \a other image will be inserted.
+     * \param other The image that will be inserted on \a img image.
+     * \param x The x-coordinate on which the left-top upper pixel of the \a other image will be inserted.
+     * \param y The y-coordinate on which the left-top upper pixel of the \a other image will be inserted.
+     * \details The function insert \a other image on the \a img image starting from the pixel with the \a x and \a y coordinates.
+     * Note, if the \a x and \a y parameters are selected in such a way that the \a other image does not fall on the \a img image, 
+     * then there will be no changes in the \a img image.
+     */
     void insert(img::Image &img, img::Image &other, int x, int y);
-
+    /** \brief Reflects the image relative to the x axis.
+     * \param img The image that will be reflected relative to the x axis.
+     */
     void reflex_x(img::Image &img);
-
+    /** \brief Reflects the image relative to the y axis.
+     * \param img The image that will be reflected relative to the y axis.
+     */
     void reflex_y(img::Image &img);
-
+    /** \brief Сhanges the image resolution.
+     * \param img The image whose resolution is being changed.
+     * \param k Multiplicity of resolution changes.
+     * \details The funtion changes the resolution of the \a img image by \a k times.
+     * Note, if the value of \a k is less than or equal to zero, the \a img image will not be changed.
+     */
     void resize(img::Image &img, double k);
-
+    /** \brief Rotates the image.
+     * \param img The image to be rotated.
+     * \param degrees The number of degrees by which the \a img image will be rotated.
+     * \details Function rotates the \a img image by \a degrees degrees from the center of the \a img image.
+     * If the value of \a degrees parameter is positive, then the rotation is counterclockwise. 
+     * If the value of the \a degrees parameter is negative, then the rotation is clockwise.
+     */
     void rotate(img::Image& img, double degrees);
 }
--- tests/processing-functions-test.cpp
+++ tests/processing-functions-test.cpp
@@ -16,22 +16,26 @@ TEST_CASE("Using negative filter"){
     proc::negative(image);
 
     SECTION("negative image"){
+        bool check_negative = true;
         for(int i = 0; i < 256; ++i){
             for(int j = 0; j < 256; ++j){
                 int R = test_pixel_map.at(i, j).R();
                 int G = test_pixel_map.at(i, j).G();
                 int B = test_pixel_map.at(i, j).B();
-                REQUIRE(image.get_map().at(i, j) == img::Color (255 - R, 255 - G, 255 - B));
+                check_negative &= image.get_map().at(i, j) == img::Color (255 - R, 255 - G, 255 - B);
             }
         }
+        REQUIRE(check_negative);
     }
     SECTION("double negative on image"){
         proc::negative(image);
+        bool check_double_negative = true;
         for(int i = 0; i < 256; ++i){
             for(int j = 0; j < 256; ++j){
-                REQUIRE(image.get_map().at(i, j) == test_pixel_map.at(i, j));
+                check_double_negative &= image.get_map().at(i, j) == test_pixel_map.at(i, j);
             }
         }
+        REQUIRE(check_double_negative);
     }
 }
 
@@ -47,35 +51,41 @@ TEST_CASE("Using crop"){
     image.get_map() = test_pixel_map;
 
     SECTION("checking square value (top and left)"){
+        bool check_square_value = true;
         for(double i = 0; i <= 90; ++i){
             for(double j = 0; j <= 90; ++j){
                 image.get_map() = test_pixel_map;
                 int rows = image.get_map().rows();
                 int columns = image.get_map().columns();
                 proc::crop(image, i, j, 0, 0);
-                REQUIRE(round(((100 - j)/100)*columns) * round(((100 - i)/100)*rows) == image.get_map().rows() * image.get_map().columns());
+                check_square_value &= round(((100 - j)/100)*columns) * round(((100 - i)/100)*rows) == image.get_map().rows() * image.get_map().columns();
             }
         }
+        REQUIRE(check_square_value);
     }
     SECTION("checking square value (bottom and right)"){
+        bool check_square_value = true;
         for(double i = 0; i <= 90; ++i){
             for(double j = 0; j <= 90; ++j){
                 image.get_map() = test_pixel_map;
                 int rows = image.get_map().rows();
                 int columns = image.get_map().columns();
                 proc::crop(image, 0, 0, j, i);
-                REQUIRE(round(((100 - j)/100)*columns) * round(((100 - i)/100)*rows) == image.get_map().rows() * image.get_map().columns());
+                check_square_value &= round(((100 - j)/100)*columns) * round(((100 - i)/100)*rows) == image.get_map().rows() * image.get_map().columns();
             }
         }
+        REQUIRE(check_square_value);
     }
     SECTION("checking crop from different sides"){
+        bool check_different_sides = true;
         for(double i = 0; i < 100; ++i){
             image.get_map() = test_pixel_map;
             proc::crop(image, i, 0, 0, 0);
             int rows = image.get_map().rows();
             int columns = image.get_map().columns();
-            REQUIRE(round(1000*(100-i)/100)*100 == rows * columns);
+            check_different_sides &= round(1000*(100-i)/100)*100 == rows * columns;
         }
+        REQUIRE(check_different_sides);
     }
 }
 
@@ -103,15 +113,17 @@ TEST_CASE("Using insert"){
     SECTION("insert everywhere"){
         for(int i = 0; i < test_rows; ++i){
             for(int j = 0; j < test_columns; ++j){
+                bool check_insert = true;
                 image.get_map() = test_pixel_map;
                 proc::insert(image, other, j, i);
                 for(int y = i; y < i + 10; ++y){
                     for(int x = j; x < j + 10; ++x){
                         if(y < test_rows && x < test_columns){
-                            REQUIRE(other.get_map().at(y - i, x - j) == image.get_map().at(y, x));
+                            check_insert &= other.get_map().at(y - i, x - j) == image.get_map().at(y, x);
                         }
                     }
                 }
+                REQUIRE(check_insert);
             }
         }
     }
@@ -129,20 +141,25 @@ TEST_CASE("Using reflexion by x"){
     img::PixelMap reflexed_pixel_map = image.get_map();
 
     SECTION("checking reflexion (x)"){
+        bool check_reflexion_x = true;
         for(int i = 0; i < rows; ++i){
             for(int j = 0; j < columns; ++j){
-                REQUIRE(reflexed_pixel_map.at(i, j) == pixel_map.at(rows - i - 1, j));
+                check_reflexion_x &= reflexed_pixel_map.at(i, j) == pixel_map.at(rows - i - 1, j);
             }
         }
+        REQUIRE(check_reflexion_x);
+
     }
     SECTION("double reflexion"){
         proc::reflex_x(image);
         img::PixelMap reflexed_pixel_map = image.get_map();
+        bool check_double_reflexion_x = true;
         for(int i = 0; i < rows; ++i){
             for(int j = 0; j < columns; ++j){
-                REQUIRE(reflexed_pixel_map.at(i, j) == pixel_map.at(i, j));
+                check_double_reflexion_x &= reflexed_pixel_map.at(i, j) == pixel_map.at(i, j);
             }
         }
+        REQUIRE(check_double_reflexion_x);
     }
 }
 
@@ -157,20 +174,24 @@ TEST_CASE("Using reflexion by y"){
     img::PixelMap reflexed_pixel_map = image.get_map();
 
     SECTION("checking reflexion (y)"){
+        bool check_reflexion_y = true;
         for(int i = 0; i < rows; ++i){
             for(int j = 0; j < columns; ++j){
-                REQUIRE(reflexed_pixel_map.at(i, j) == pixel_map.at(i, columns - j - 1));
+                check_reflexion_y &= reflexed_pixel_map.at(i, j) == pixel_map.at(i, columns - j - 1);
             }
         }
+        REQUIRE(check_reflexion_y);
     }
     SECTION("double reflexion"){
         proc::reflex_y(image);
         img::PixelMap reflexed_pixel_map = image.get_map();
+        bool check_double_reflexion_y = true;
         for(int i = 0; i < rows; ++i){
             for(int j = 0; j < columns; ++j){
-                REQUIRE(reflexed_pixel_map.at(i, j) == pixel_map.at(i, j));
+                check_double_reflexion_y &= reflexed_pixel_map.at(i, j) == pixel_map.at(i, j);
             }
         }
+        REQUIRE(check_double_reflexion_y);
     }
 }
 
@@ -181,24 +202,28 @@ TEST_CASE("Resizing"){
     img::PixelMap pixel_map = image.get_map();
     int rows = pixel_map.rows();
     int columns = pixel_map.columns();
-
+    
     SECTION("checking size"){
         for(double k = 0.1; k <= 10; k += 0.1){
+            bool check_square = true;
             image.get_map() = pixel_map;
             proc::resize(image, k);
-            REQUIRE(image.get_map().rows() == round(rows * k));
-            REQUIRE(image.get_map().columns() == round(columns * k));
+            check_square &= image.get_map().rows() == round(rows * k);
+            check_square &= image.get_map().columns() == round(columns * k);
+            REQUIRE(check_square);
         }
     }
     SECTION("checking integer k"){
         for(double k = 1; k <= 10; ++k){
+            bool check_integer = true;
             proc::resize(image, k);
             proc::resize(image, 1/k);
             for(int i = 0; i < rows; ++i){
                 for(int j = 0; j < columns; ++j){
-                    REQUIRE(pixel_map.at(i, j) == image.get_map().at(i, j));
+                    check_integer &= pixel_map.at(i, j) == image.get_map().at(i, j);
                 }
             }
+            REQUIRE(check_integer);
         }
     }
 }
@@ -206,31 +231,66 @@ TEST_CASE("Resizing"){
 TEST_CASE("test rotate"){
     using img_t = img::PPMImage;
     img_t image;
-    image.read("resources/sign_1.ppm");
+    image.read("resources/west_1.ppm");
     img::PixelMap pixel_map = image.get_map();
     int rows = pixel_map.rows();
     int columns = pixel_map.columns();
 
-    img_t img0 = image;
-    img_t img90 = image;
-    img_t img180 = image;
-    img_t img270 = image;
+    SECTION("90x angles"){
+        int counter0 = 0;
+        int counter90 = 0;
+        int counter180 = 0;
+        int counter270 = 0;
+        img_t img0 = image;
+        img_t img90 = image;
+        img_t img180 = image;
+        img_t img270 = image;
+        proc::rotate(img0, 0);
+        proc::rotate(img90, 90);
+        proc::rotate(img180, 180);
+        proc::rotate(img270, 270);
 
-    // SECTION("90x angles"){
-    //     for(int i = 0; i < rows; ++i){
-    //         for(int j = 0; j < columns; ++j){
-    //             proc::rotate(img0, 0);
-    //             proc::rotate(img90, 90);
-    //             proc::rotate(img180, 180);
-    //             proc::rotate(img270, 270);
-    //             REQUIRE(image.get_map().at(i, j) == img0.get_map().at(i, j));
-    //             std::cout << j << " " << i << std::endl;
-    //             REQUIRE(image.get_map().at(i, j) == img90.get_map().at(columns - j, i));
-    //             REQUIRE(image.get_map().at(i, j) == img180.get_map().at(rows - i, columns - j));
-    //             REQUIRE(image.get_map().at(i, j) == img270.get_map().at(j, rows - i));
-    //         }
-    //     }
-    // }
-
-    image.write("resources/negative.ppm");
+        for(int i = 0; i < rows; ++i){
+            for(int j = 0; j < columns; ++j){
+                if(image.get_map().at(i, j) == img0.get_map().at(i, j)){
+                    ++counter0;
+                }
+                if(image.get_map().at(i, j) == img90.get_map().at(columns - j - 1, i)){
+                    ++counter90;
+                }
+                if(image.get_map().at(i, j) == img180.get_map().at(rows - i - 1, columns - j - 1)){
+                    ++counter180;
+                }
+                if(image.get_map().at(i, j) == img270.get_map().at(j, rows - i - 1)){
+                    ++counter270;
+                }
+            }
+        }
+        REQUIRE(counter0 == rows*columns);
+        REQUIRE(counter90 == rows*columns);
+        REQUIRE(counter180 == rows*columns);
+        REQUIRE(counter270 == rows*columns);
+    }
+    SECTION("the same modulo 360"){
+        for(double degrees = 0; degrees < 15; ++degrees){
+            img_t img0 = image;
+            img_t img_360 = image;
+            img_t img360 = image;
+            proc::rotate(img0, degrees);
+            proc::rotate(img_360, degrees - 360);
+            proc::rotate(img360, degrees + 360);
+            rows = img0.get_map().rows();
+            columns = img0.get_map().columns();
+            bool check_360 = true;
+            bool check360 = true;
+            for(int i = 0; i < rows; ++i){
+                for(int j = 0; j < columns; ++j){
+                    check_360 &= img0.get_map().at(i, j) == img_360.get_map().at(i, j);
+                    check360 &= img0.get_map().at(i, j) == img360.get_map().at(i, j);
+                }
+            }
+            REQUIRE(check_360);
+            REQUIRE(check360);
+        }
+    }
 }
