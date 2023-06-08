846793ad646dd6341fb09fdd916e285dd5d98121
AshFungor <evgenynarkhov2@gmail.com>
2023-06-09 01:00:21 +0300

    Shrinking image test samples

--- tests/image-image-test.cpp
+++ tests/image-image-test.cpp
@@ -11,13 +11,9 @@
 TEST_CASE("PPM reading and writing in <Image> class", "[base]") {
     using img_t = img::PPMImage;
     const std::string files[] {
-        "boxes_1.ppm",      "house_1.ppm",
-        "moreboxes_1.ppm",  "sign_1.ppm",
-        "stop_1.ppm",       "stop_2.ppm",
-        "tree_1.ppm",       "west_1.ppm",
-        "boxes_2.ppm",      "house_2.ppm",
-        "moreboxes_2.ppm",  "sign_2.ppm",
-        "tree_2.ppm",       "west_2.ppm",
+        "boxes.ppm",      "house.ppm",
+        "stop.ppm",       "sign.ppm",
+        "tree.ppm",       "west.ppm",
     };
 
 
@@ -45,20 +41,20 @@ TEST_CASE("PPM reading and writing in <Image> class", "[base]") {
 TEST_CASE("PNG reading and writing in <Image> class", "[base]") {
     using img_t = img::PNGImage;
     const std::string files[] {
-        "good_normal_tiny-rgb-gray.png",
-        "sample-bumblebee-400x300.png",
-        "sample-clouds2-400x300.png",
-        "sample-hut-400x300.png"
+        "simple.png",
+        "hut.png",
+        "clouds.png",
+        "bumblebee.png"
     };
 
     for (auto& img_path : files) {
         auto image_1 = std::make_unique<img_t>(img_t{});
         auto image_2 = std::make_unique<img_t>(img_t{});
         image_1->read(std::string{"resources/"}.append(img_path));
-        // REQUIRE(image_1->good());
+        REQUIRE(image_1->good());
         image_1->write(std::string{"resources/"}.append("result.ppm"));
         image_2->read(std::string{"resources/"}.append("result.ppm"));
-        // REQUIRE((image_1->good() && image_2->good()));
+        REQUIRE((image_1->good() && image_2->good()));
         img::PixelMap& img_1_map {image_1->get_map()}, img_2_map {image_2->get_map()};
         REQUIRE(img_1_map.rows() == img_2_map.rows());
         REQUIRE(img_1_map.columns() == img_2_map.columns());
