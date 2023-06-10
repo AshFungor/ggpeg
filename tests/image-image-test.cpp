76111370c4dcfaafecd5975a8d0d978a84e71a26
AshFungor <evgenynarkhov2@gmail.com>
2023-06-10 21:55:49 +0300

    Feature: Compression; Finished tests for compression; Bugfix: adler32 is now calculated correctly

--- tests/image-image-test.cpp
+++ tests/image-image-test.cpp
@@ -55,8 +55,8 @@ TEST_CASE("PNG reading and writing in <Image> class", "[base]") {
         auto image_2 = std::make_unique<img_t>(img_t{});
         image_1->read(std::string{"resources/"}.append(img_path));
         REQUIRE(image_1->good());
-        image_1->write(std::string{"resources/"}.append("result.ppm"));
-        image_2->read(std::string{"resources/"}.append("result.ppm"));
+        image_1->write(std::string{"resources/"}.append("result.png"));
+        image_2->read(std::string{"resources/"}.append("result.png"));
         REQUIRE((image_1->good() && image_2->good()));
         img::PixelMap& img_1_map {image_1->get_map()}, img_2_map {image_2->get_map()};
         REQUIRE(img_1_map.rows() == img_2_map.rows());
